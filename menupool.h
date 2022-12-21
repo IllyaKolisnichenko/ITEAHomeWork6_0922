87296bdd2d894e5db7ba20a95ae12c8c82c96fda
danilpocht <danapoctamcev@gmail.com>
2022-12-21 19:53:05 +0200

    homework

---
+++ menupool.h
@@ -0,0 +1,14 @@
+#pragma once
+char mainMenu();
+
+void menuPlayers();
+
+void menuChoiseWater();
+
+void createPlayer();
+
+int existPlayers();
+
+int exitPlay();
+
+bool readFromFile(std::vector<std::string>& data);
